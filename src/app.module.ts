8bf41387c7cb361de8e697cd20090a7afe97bdb8
tianxiangtee <47767907+tianxiangtee@users.noreply.github.com>
2023-01-21 04:50:22 +0000

    Generate template resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TemplateModule } from './template/template.module';
 
 @Module({
-  imports: [],
+  imports: [TemplateModule],
   controllers: [AppController],
   providers: [AppService],
 })
