77e93509137c5ce16781c96173f4137110c738db
tianxiangtee <47767907+tianxiangtee@users.noreply.github.com>
2023-01-27 04:57:57 +0000

    Init pre save on mongoose

--- src/template/template.module.ts
+++ src/template/template.module.ts
@@ -6,11 +6,28 @@ import { Template, TemplateSchema } from './entities/template.entity';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([
-      { name: Template.name, schema: TemplateSchema },
+    MongooseModule.forFeatureAsync([
+      {
+        name: Template.name,
+        useFactory: () => {
+          const schema = TemplateSchema;
+          // schema.path('_id').set(function (newVal) {
+          //   console.log('newVal', newVal);
+          //   console.log('ori', this);
+          // });
+          schema.post('init', function () {
+            const temp = this.toObject();
+            console.log('ori', temp);
+          });
+          schema.pre<Template>('save', async function () {
+            console.log('pre save mongo');
+          });
+          return schema;
+        },
+      },
     ]),
   ],
   controllers: [TemplateController],
   providers: [TemplateService],
 })
-export class TemplateModule {}
+export class TemplateModule { }
