8e865bb41861e6d4804635da8376df3710ffc25c
tianxiangtee <47767907+tianxiangtee@users.noreply.github.com>
2023-10-25 03:08:47 +0000

    Refractor the addModule

--- GENERATOR/src/Utility/addModule.js
+++ GENERATOR/src/Utility/addModule.js
@@ -1,54 +1,62 @@
-import fs from 'fs'
-import prettier from 'prettier'
-import path from 'path'
+import fs from 'fs/promises'; // Import fs.promises for asynchronous file operations
+import prettier from 'prettier';
+import path from 'path';
 import { fileURLToPath } from 'url';
 import { snakeToKebabCase, snakeToPascalCase } from './textTransform.js';
-
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const currentDirectory = process.cwd()
-const defaultPath = path.join(currentDirectory, 'src');
-const appModulePath = path.join(defaultPath, "app.module.ts");
-console.log('appModulePath', appModulePath)
-
 async function addModule(moduleName, folderPath = null) {
-    // Define the new module name
-    const folderName = snakeToPascalCase(moduleName); // Replace with your desired module name
-    const newModuleName = folderPath == null? folderName : snakeToPascalCase(moduleName + folderPath); // Replace with your desired module name
-    const moduleFilename = folderPath == null ? snakeToKebabCase(moduleName): snakeToKebabCase(moduleName) + "-" + folderPath.toLowerCase()
-    const folderPathStr = folderPath == null ? "" :folderPath.toLowerCase()+ "/" 
+    try {
+        // Get the current working directory
+        const currentDirectory = process.cwd();
+        // Define the default path and the path to the app.module.ts file
+        const defaultPath = path.join(currentDirectory, 'src');
+        const appModulePath = path.join(defaultPath, 'app.module.ts');
 
-    // Read the content of the app.module.ts file
-    let appModuleContent = fs.readFileSync(appModulePath, 'utf8');
+        console.log('appModulePath', appModulePath);
 
-    // Construct the import statement
-    const moduleImportPath = `./module/${folderName}/${folderPathStr}${moduleFilename}.module`;
-    const importStatement = `import { ${ newModuleName}Module } from '${moduleImportPath}';\n`;
+        // Convert the module name to PascalCase
+        const folderName = snakeToPascalCase(moduleName);
+        // Calculate the new module name based on provided parameters
+        const newModuleName = folderPath == null ? folderName : snakeToPascalCase(`${moduleName}${folderPath}`);
+        // Calculate the module filename based on provided parameters
+        const moduleFilename = folderPath == null ? snakeToKebabCase(moduleName) : snakeToKebabCase(`${moduleName}-${folderPath.toLowerCase()}`);
+        // Create a folder path string based on the parameters
+        const folderPathStr = folderPath == null ? '' : `${folderPath.toLowerCase()}/`;
 
-    // Find the position to insert the new import statement (after MongooseModule and before the comments)
-    const mongoosePosition = appModuleContent.indexOf('MongooseModule.forRoot(process.env.MONGO_CONNECTION_STRING)');
-    const commentsPosition = appModuleContent.indexOf('// TemplateModule');
+        // Read the content of the app.module.ts file
+        let appModuleContent = await fs.readFile(appModulePath, 'utf8');
 
-    if (mongoosePosition !== -1 && commentsPosition !== -1) {
-        // Insert the newModuleName statement between MongooseModule and the comments
-        appModuleContent =
-            appModuleContent.slice(0, mongoosePosition) + `${newModuleName}Module,\n` +
-            appModuleContent.slice(mongoosePosition);
+        // Define the import statement for the new module
+        const moduleImportPath = `./module/${folderName}/${folderPathStr}${moduleFilename}.module`;
+        const importStatement = `import { ${newModuleName}Module } from '${moduleImportPath}';\n`;
+
+        // Find the position to insert the new import statement (between MongooseModule and the comments)
+        const mongoosePosition = appModuleContent.indexOf('MongooseModule.forRoot(process.env.MONGO_CONNECTION_STRING)');
+        const commentsPosition = appModuleContent.indexOf('// TemplateModule');
+
+        if (mongoosePosition !== -1 && commentsPosition !== -1) {
+            // Insert the newModuleName statement between MongooseModule and the comments
+            appModuleContent =
+                appModuleContent.slice(0, mongoosePosition) + `${newModuleName}Module,\n` +
+                appModuleContent.slice(mongoosePosition);
+        }
+
+        // Insert the import statement at the top of the file
+        appModuleContent = importStatement + appModuleContent;
+
+        // Reformat the code using Prettier with the TypeScript parser
+        const formattedCode = await prettier.format(appModuleContent, { parser: 'typescript' });
+
+        // Save the updated app.module.ts file
+        await fs.writeFile(appModulePath, formattedCode, 'utf8');
+
+        console.log('New module added to app.module.ts.');
+    } catch (error) {
+        console.error('An error occurred:', error);
     }
-
-    // Insert the import statement at the top
-    appModuleContent = importStatement + appModuleContent;
-
-    // Reformat the code using Prettier
-    const formattedCode = await prettier.format(appModuleContent, { parser: 'typescript' });
-
-    // Save the updated app.module.ts file
-    fs.writeFileSync(appModulePath, formattedCode, 'utf8');
-
-    console.log('New module added to app.module.ts.');
-
 }
 
 export default addModule;
